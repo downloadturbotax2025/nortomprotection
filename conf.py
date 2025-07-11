b560b600feed4f135d02c8b77ff40fefab14a7a8
downloadturbotax2025 <jjiku867@gmail.com>
2025-07-11 12:47:48 +0530

    Update conf.py

--- conf.py
+++ conf.py
@@ -16,7 +16,7 @@ import sys
 
 # -- Project information -----------------------------------------------------
 
-project = 'Disneyplus TV'
+project = 'norton setup'
 
 
 # The full version, including alpha/beta/rc tags
